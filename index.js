2baaa9e796d9998bb902acaf047dda747e66f38c
Ramesh Amadoru <rameshamadoru@Rameshs-MacBook-Air.local>
2019-07-18 02:49:49 -0400

    Updated the Hello message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.use('*', (req, res, next) => {
-res.send('<h1>!-- Hello From Ramesh --!</h1>');
+res.send('<h1>!-- Hello From Ramesh Node App --!</h1>');
 });
 
 app.listen(4000, () => {
